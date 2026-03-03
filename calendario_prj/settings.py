913c0fb8a0cc1394bd82b6bbd4dd6061cdeace14
Pedro Bolig <pedro.bolig@gmail.com>
2026-03-03 19:27:50 -0300

    Fix 403 local: Desactivar SessionAuth de DRF

--- calendario_prj/settings.py
+++ calendario_prj/settings.py
@@ -149,7 +149,6 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
